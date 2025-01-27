c0568c6ebfa3bc84130fdc99491afa5a588e10c0
KDVSS <dhanavenkatasivasai@gmail.com>
2025-01-27 23:53:32 +0100

    Added Scripts folder

---
+++ scripts/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Hello Papa" # Mouli papa
+
+if [ 2 -gt 5 ]; then
+	echo "2 is greater than 5"
+else
+	echo "2 is less than 5"
+fi
+
+for i in 1 2 3; do
+	echo "Number: $i"
+done	
+ 
